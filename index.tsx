5ab389f1fbc2f8d91df5c8dcb682a3c522004410
Victor Calvo <victorcalvo12@gmail.com>
2024-05-13 23:38:55 -0500

    Moved styles to static file Added 404 and 500 routes Added deno fmt config

--- index.tsx
+++ index.tsx
@@ -1,47 +1,23 @@
-import render from 'preact-render-to-string';
+import render from "preact-render-to-string";
 
 const App = () => {
-	return <html>
-		<head>
-			<meta charset="UTF-8"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-			<style>{`
-				:root {
-					color-scheme: light dark;
-					
-					@media (prefers-color-scheme: dark){
-						--theme-color: black;
-						--theme-font-color: white;
-					}
+	return (
+		<html>
+			<head>
+				<meta charset="UTF-8" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link href="index.css" rel="stylesheet"></link>
+			</head>
+			<body>
+				<div id="hero-welcome">Hello, World.</div>
+			</body>
+		</html>
+	);
+};
 
-					@media (prefers-color-scheme: light){
-						--theme-color: white;
-						--theme-font-color: black;
-					}
-				}
-				body {
-					background-color: var(--theme-color);
-					color: var(--theme-font-color);
-					font-family: ui-sans-serif, system-ui, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
-				}
-				#hero-welcome {
-					font-size: 2.5rem;
-					margin: auto;
-					max-width: 50rem;
-					margin-top: 5rem;
-					width: 87.5%;
-				}
-			`}</style>
-		</head>
-		<body>
-			<div id="hero-welcome">Hello, World.</div>
-		</body>
-	</html>
-}
-
-export function loadPage(){
+export function loadPage() {
 	return `
 		<!DOCTYPE html>
-		${render( <App/> )}
-	`
+		${render(<App />)}
+	`;
 }
--- server.ts
+++ server.ts
@@ -1,9 +1,24 @@
 import { loadPage } from "./index.tsx";
 
-Deno.serve(( _request: Request ) => {
-	return new Response( loadPage(), {
-		headers: {
-			"Content-Type": "text/html; charset=utf-8"
+Deno.serve(async (_request: Request) => {
+	try {
+		const indexPattern = new URLPattern({ pathname: "/" });
+		const stylePattern = new URLPattern({ pathname: "/index.css" });
+
+		if (stylePattern.test(_request.url)) {
+			const file = await Deno.open("index.css");
+
+			return new Response(file.readable);
+		} else if (indexPattern.test(_request.url)) {
+			return new Response(loadPage(), {
+				headers: {
+					"Content-Type": "text/html; charset=utf-8",
+				},
+			});
+		} else {
+			return new Response("404 Not Found", { status: 404 });
 		}
-	} );
+	} catch {
+		return new Response("500 Internal Server Error", { status: 500 });
+	}
 });
